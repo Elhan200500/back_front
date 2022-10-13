c038060229d1bc3b03cd9de9c6cb8be200836a6f
Elhan200500 <kazbaevelhan@gmail.com>
2022-10-13 21:11:50 +0600

    commiting

---
+++ news/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (
+    my_api_view,
+    newslist_view,
+    detail_list_view,
+    news_create_view,
+    news_update_view,
+    news_delete_view
+)
+
+
+urlpatterns = [
+    path('', my_api_view),
+    path('news/', newslist_view),
+    path('news-detail/<int:id>/', detail_list_view),
+    path('news-create', news_create_view),
+    path('news-update/<int:id>/', news_update_view),
+    path('news-delete/<int:id>/', news_delete_view)
+]
